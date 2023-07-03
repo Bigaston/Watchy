cb7bc2716706c6d4b40e80ae0d2c966a65138c71
Bigaston <bigaston1@gmail.com>
2023-07-03 11:44:40 +0200

    Resize sprite

--- src/editor/editorView.ts
+++ src/editor/editorView.ts
@@ -1,6 +1,6 @@
 import * as PIXI from "pixi.js";
-import { WImage, WImageStatus } from "../types/types";
-import { loadGame } from "./storage";
+import { WGameDescription, WImage, WImageStatus } from "../types/types";
+import { loadGame, saveGame } from "./storage";
 import { PALETTE } from "../types/colorPalette";
 
 const width = 900;
@@ -11,7 +11,15 @@ const rendererElement = document.getElementById("renderer")!;
 let app: PIXI.Application;
 
 let selectedSprite: WImage | undefined;
-let selectSquare: PIXI.Graphics;
+let isSelectedSpriteDragged = false;
+let isSelectedResized = false;
+
+let mouseOffset = { x: 0, y: 0 };
+let mousePosition = { x: 0, y: 0 };
+
+let hoverSelector = new PIXI.Container();
+
+let game: WGameDescription;
 
 export const sprites: WImage[] = [];
 
@@ -39,12 +47,13 @@ export function initEditorView() {
 
   background.on("pointerdown", (_event) => {
     selectedSprite = undefined;
+    hoverSelector.removeChildren();
   });
 
   app.stage.addChild(background);
 
   // Load Game Sprites
-  let game = loadGame();
+  game = loadGame();
 
   game.images.forEach((image) => {
     let spr = new PIXI.Sprite(PIXI.Texture.from(image.path));
@@ -68,6 +77,30 @@ export function initEditorView() {
 
     spr.on("pointerdown", (event) => {
       onSelectSprite(wImage);
+      isSelectedSpriteDragged = true;
+
+      mouseOffset.x = event.data.global.x - spr.x;
+      mouseOffset.y = event.data.global.y - spr.y;
+
+      spr.cursor = "move";
+
+      initSelectionBox(spr, wImage);
+    });
+
+    spr.on("pointerup", (event) => {
+      isSelectedSpriteDragged = false;
+
+      game.images = game.images.map((image) => {
+        if (image.id === wImage.id) {
+          image.x = spr.x;
+          image.y = spr.y;
+        }
+        return image;
+      });
+
+      saveGame(game);
+
+      spr.cursor = "pointer";
     });
 
     spr.cursor = "pointer";
@@ -75,10 +108,20 @@ export function initEditorView() {
     app.stage.addChild(spr);
   });
 
-  // Initialize Select Square
-  selectSquare = new PIXI.Graphics();
+  // Handle Mouse Position
+  (app.view as unknown as HTMLElement).addEventListener(
+    "mousemove",
+    (event) => {
+      mousePosition.x =
+        (event.offsetX / (app.view as unknown as HTMLElement).clientWidth) *
+        width;
+      mousePosition.y =
+        (event.offsetY / (app.view as unknown as HTMLElement).clientHeight) *
+        height;
+    }
+  );
 
-  app.stage.addChild(selectSquare);
+  app.stage.addChild(hoverSelector);
 }
 
 function onSelectSprite(sprite: WImage) {
@@ -87,16 +130,78 @@ function onSelectSprite(sprite: WImage) {
 
 function ticker(delta: number) {
   if (selectedSprite) {
-    selectSquare.lineStyle(2, 0x000000, 1);
-    selectSquare.drawRect(
-      selectedSprite.sprite.x,
-      selectedSprite.sprite.y,
-      selectedSprite.sprite.width,
-      selectedSprite.sprite.height
-    );
-  } else {
-    selectSquare.clear();
+    hoverSelector.x = selectedSprite.sprite.x;
+    hoverSelector.y = selectedSprite.sprite.y;
+
+    if (isSelectedSpriteDragged) {
+      selectedSprite.sprite.x = mousePosition.x - mouseOffset.x;
+      selectedSprite.sprite.y = mousePosition.y - mouseOffset.y;
+    }
+
+    if (isSelectedResized) {
+      selectedSprite.sprite.width = mousePosition.x - selectedSprite.sprite.x;
+      selectedSprite.sprite.height = mousePosition.y - selectedSprite.sprite.y;
+
+      let square = hoverSelector.getChildAt(0) as PIXI.Graphics;
+      square.clear();
+      square.lineStyle(2, 0x000000, 1);
+      square.drawRect(
+        0,
+        0,
+        selectedSprite.sprite.width,
+        selectedSprite.sprite.height
+      );
+
+      (hoverSelector.getChildAt(1) as PIXI.Graphics).x =
+        selectedSprite.sprite.width;
+      (hoverSelector.getChildAt(1) as PIXI.Graphics).y =
+        selectedSprite.sprite.height;
+    }
   }
+}
+
+function initSelectionBox(spr: PIXI.Sprite, wSpr: WImage) {
+  hoverSelector.removeChildren();
+
+  let square = new PIXI.Graphics();
+  square.clear();
+  square.lineStyle(2, 0x000000, 1);
+  square.drawRect(0, 0, spr.width, spr.height);
+
+  let sphere = new PIXI.Graphics();
+  sphere.lineStyle(0);
+  sphere.beginFill(0xff0000, 0.5);
+  sphere.drawCircle(0, 0, 10);
+
+  sphere.x = spr.width;
+  sphere.y = spr.height;
+
+  sphere.eventMode = "static";
+  sphere.cursor = "nwse-resize";
+
+  sphere.on("pointerdown", (event) => {
+    isSelectedResized = true;
+
+    mouseOffset.x = event.data.global.x - spr.width;
+    mouseOffset.y = event.data.global.y - spr.height;
+  });
+
+  sphere.on("pointerup", (_event) => {
+    isSelectedResized = false;
+
+    game.images = game.images.map((image) => {
+      if (image.id === wSpr.id) {
+        image.width = spr.width;
+        image.height = spr.height;
+      }
+      return image;
+    });
+
+    saveGame(game);
+  });
+
+  hoverSelector.addChild(square);
+  hoverSelector.addChild(sphere);
 }
 
 function resize() {
